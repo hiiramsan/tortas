7d6cae6854aa239d684388b82dae736823036be8
Carlos H Sánchez <155609143+hiiramsan@users.noreply.github.com>
2024-03-18 14:43:17 -0700

    Presentacion
    codigo pa la prese a la verga

---
+++ TortasPresentacion/src/main/java/com/mycompany/tortaspresentacion/TortasPresentacion.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tortaspresentacion;
+
+/**
+ *
+ * @author carlo
+ */
+public class TortasPresentacion {
+
+    public static void main(String[] args) {
+        System.out.println("jajajajajja!");
+    }
+}
